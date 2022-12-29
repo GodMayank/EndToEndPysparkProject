edecdc3b80129cc3a4c5c46de06d167d50fd5589
DataMaking <mayank612512@gmail.com>
2022-12-30 01:02:54 +0530

    commit

--- Code/ETL_Code.py
+++ Code/ETL_Code.py
@@ -1,1 +1,14 @@
 from pyspark.sql import SparkSession
+
+jobname = "ETL_Code"
+spark = SparkSession.builder.master("local[*]").appName(jobname).getOrCreate()
+
+file_path = "Data/airports.csv"
+airports = spark.read.csv(file_path, header=True)
+airports.show()
+
+type(airports)
+
+flights = spark.read.csv("Data/flights_small.csv", header = True)
+flights.show()
+
